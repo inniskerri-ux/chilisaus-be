a9483048e0ee85a6a29696cc2f0726f4379503bb
Nathan Austin <nath.austin@gmail.com>
2026-02-24 23:32:02 +0100

    feat: configure middleware to always prefix locale and handle root redirect

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['nl', 'en', 'fr', 'cs', 'es', 'de', 'pl'],
   defaultLocale: 'en',
+  localePrefix: 'always',
+  localeDetection: true // This will detect browser lang, but defaultLocale is 'en'
 });
 
 export const config = {
